abb89c2ff7902f08eab275e6df79273bb540c723
nsubiron <nsubiron@gmail.com>
2018-06-03 00:06:39 +0200

    Initial commit

---
+++ include/strm/strm.h
@@ -0,0 +1,5 @@
+#pragma once
+
+static inline auto say_hello() {
+  return "Hello!";
+}
---
+++ test/test_the_test.cpp
@@ -0,0 +1,9 @@
+#include <strm/strm.h>
+
+#include <gtest/gtest.h>
+
+#include <iostream>
+
+TEST(meta, test_google_test) {
+  std::cout << "Hello!\n";
+}
